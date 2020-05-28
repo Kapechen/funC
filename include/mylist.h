80a7477dede2278f4521d5e822e386952ca97d13
kapechen <1204583876@qq.com>
2020-05-28 23:19:24 +0800

    完善了双向List链表

--- include/mylist.h
+++ include/mylist.h
@@ -1,17 +1,45 @@
 #ifndef  MYLIST_H_
 #define MYLIST_H_
+#include <iostream>
+#include <exception>
+using namespace std;
 /*
 **Title:自己实现List双向链表
 **Author:kapechen
 **Version:v0.1
 **Log:
-*----------------------------------------------------------------
-*----|	index	|	Date	|	Content		|	Problems      |	
-*		  1		  2020/5/20    基本框架构建    边界检测细节未完成
+*----------------------------------------------------------------------
+*----|	index	|	Date	|	Content				|	Problems      |	
+*		  1		  2020/5/20    基本框架构建			边界检测细节未完成
+*		  2       2020/5/28    增加了注释和边界检测类      尚未发现 
 *
-*
-*----------------------------------------------------------------
+*---------------------------------------------------------------------
 */
+/*
+|——List类
+	|
+	|——Node结构体
+	|——const_iterator类
+			|——iterator类
+*/
+class IteratorOutOfBoundsException:public exception
+{
+public:
+	IteratorOutOfBoundsException()
+	{
+		cout << "Iterator Out Of Bounds!" << endl;
+	}
+	IteratorOutOfBoundsException(const char* message) :message_{ message }
+	{}
+	IteratorOutOfBoundsException(consy IteratorOutOfBoundsException &other):message_{other.message_}
+	{}
+	const char* what()const
+	{
+		return message_.c_str();
+	}
+private:
+	string message_;
+};
 template <typename Object>
 class List
 {
@@ -25,20 +53,36 @@ private:
 		Node(Object&& d, Node* p = nullptr, Node* n = nullptr):data{std::move(d)},prev{p},next{n}{ }
 	};
 public:
+	/*迭代器类的定义:const_iterator类
+						|
+						 ——iterator类*/
 	class const_iterator
 	{
 	public:
 		const_iterator():current{nullptr}
 		{}
+		/// <summary>
+		/// 取得迭代器当前指向节点值的引用
+		/// </summary>
+		/// <returns>当前节点的值的引用</returns>
 		const Object&operator*()const
 		{
 			return retrieve();
 		}
+		/// <summary>
+		/// 迭代器前置加加
+		/// </summary>
+		/// <returns>const迭代器的引用</returns>
 		const_iterator& operator++()//++itr
 		{
 			current = current->next;
 			return *this;
 		}
+		/// <summary>
+		/// 迭代器后置加加
+		/// </summary>
+		/// <param name="int">int</param>
+		/// <returns>const迭代器</returns>
 		const_iterator operator++(int)//itr++
 		{
 			const_iterator old = *this;
@@ -55,7 +99,7 @@ public:
 		}
 
 	protected:
-		const List<Object>* theList;
+		const List<Object>* theList;//指向当前List的const指针
 		Node* current;
 		Object &retrieve()const
 		{
@@ -66,7 +110,7 @@ public:
 		void assertIsValid()const
 		{
 			if (theList == nullptr || current == nullptr || current == theList->head)
-				throw IteratorOutOfBoundsException{};
+				throw IteratorOutOfBoundsException{};//C++11统一风格 为了统一针对vector类型初始化对象语法
 		}
 		friend class List<Object>;
 	};
@@ -99,6 +143,7 @@ public:
 		friend class List<Object>;
 	};
 public:
+	/*List类基本函数部分*/
 	List(){
 		init();
 	}
@@ -131,6 +176,7 @@ public:
 		std::swap(tail, rhs.tail);
 		return *this;
 	}
+	/*迭代器相关操作*/
 	iterator begin()
 	{
 		return {
@@ -149,6 +195,43 @@ public:
 	{
 		return { tail };
 	}
+	//itr之前插入x
+	iterator insert(iterator itr, const Object& x)
+	{
+		itr.assertIsValid();
+		if (itr.theList != this)
+			throw IteratorMismatchException{};
+		Node* p = itr.current;
+		theSize++;
+		return { p->prev = p->prev->next = new Node{x,p->prev,p} };
+	}
+	//itr之前插入x
+	iterator insert(iterator itr, Object&& x)
+	{
+		itr.assertIsValid();
+		if (itr.theList != this)
+			throw IteratorMismatchException{};
+		Node* p = itr.current;
+		theSize++;
+		return { p->prev = p->prev->next = new Node{std::move(x),p->prev,p} };
+	}
+	iterator erase(iterator itr)
+	{
+		Node* p = itr.current;
+		iterator retVal{ p->next };
+		p->prev->next = p->next;
+		p->next->prev = p->prev;
+		delete p;
+		theSize--;
+		return retVal;
+	}
+	iterator erase(iterator from, iterator to)
+	{
+		for (iterator itr = from; itr != to;)
+			itr = erase(itr);
+		return to;
+	}
+	/*List类常用函数*/
 	int size()const
 	{
 		return theSize;
@@ -202,43 +285,9 @@ public:
 	{
 		erase(--end());
 	}
-	//itr之前插入x
-	iterator insert(iterator itr, const Object& x)
-	{
-		itr.assertIsValid();
-		if (itr.theList != this)
-			throw IteratorMismatchException{};
-		Node* p = itr.current;
-		theSize++;
-		return { p->prev = p->prev->next = new Node{x,p->prev,p} };
-	}
-	//itr之前插入x
-	iterator insert(iterator itr, Object&& x)
-	{
-		itr.assertIsValid();
-		if (itr.theList != this)
-			throw IteratorMismatchException{};
-		Node* p = itr.current;
-		theSize++;
-		return { p->prev = p->prev->next = new Node{std::move(x),p->prev,p} };
-	}
-	iterator erase(iterator itr)
-	{
-		Node* p = itr.current;
-		iterator retVal{ p->next };
-		p->prev->next = p->next;
-		p->next->prev = p->prev;
-		delete p;
-		theSize--;
-		return retVal;
-	}
-	iterator erase(iterator from, iterator to)
-	{
-		for (iterator itr = from; itr != to;)
-			itr = erase(itr);
-		return to;
-	}
+	
 private:
+	/*私有成员，包括初始化函数*/
 	int theSize;
 	Node* head;
 	Node* tail;
